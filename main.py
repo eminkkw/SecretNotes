a00be38f76273802082113cda06a590aca69682f
Emin Karka <muhammetemin.karka1@gmail.com>
2023-06-22 17:25:19 +0300

    Uygulama arayüzü bitti

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+-> Herhangi bir not girildiğinde saklayacak bir uygulama
+-> En yukarıda bir resim
+-> Bir label ve ismi "Enter your title" olacak
+-> Altında bir entry
+-> Onunda altında bir tane daha label ve ismi "Enter your secret"
+-> Ardından bir text
+-> Altında bir adet label daha ve ismi "Enter master key "
+-> Ardından bir adet daha entry
+-> Sonra da iki adet alt alta button lar
+'''
